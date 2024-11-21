1dd30ccda376d80fe285541397674a132d6c413c
Teekay <tkmaile96@gmail.com>
2024-11-21 13:58:59 +0200

    Added initial implementation of Blog posts

---
+++ src/BlogPosts.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+
+const BlogPosts = () => {
+    const [post, setPosts] = useState([]) // Declare a new state variable, which we'll call "post"
+    const [error, setError] = useState(null); // Declare a new state variable, which we'll call "error"
+    return (
+
+    )
+}
+export default BlogPosts;
